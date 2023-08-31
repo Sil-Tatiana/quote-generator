89b503123cb960f448fac573d016f8132b85075b
Silvia Neves <siltatiana.sn@gmail.com>
2023-08-31 23:02:01 +0100

    api call

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let allQuotes = [];
 
+function randomQuote() {
+  const quoteNum = Math.floor(Math.random() * allQuotes.length);
+  console.log(quoteNum);
+  const quote = allQuotes[quoteNum];
+  console.log(quote);
+}
+
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    allQuotes = await response.json();
+    randomQuote();
+  } catch (error) {}
+}
+
+getQuotes();
