7e2bf305ef95024eab58fe34c0c71ca1921c9dd8
Silvia Neves <siltatiana.sn@gmail.com>
2023-09-03 15:14:33 +0100

    Random quote Generator-final commit

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
+let newQuoteBtn = document.getElementById("new-quote");
+newQuoteBtn.addEventListener("click", getQuotes);
+let twitterBtn = document.getElementById("twitter");
+twitterBtn.addEventListener("click", twitterPost);
+let quoteText = document.getElementById("quote");
+let authorText = document.getElementById("author");
+let loader = document.getElementById("loader");
+let quoteContainer = document.getElementById("random-quote-container");
 let allQuotes = [];
 
+function showLoader() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function showQuote() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
 function randomQuote() {
+  showLoader();
   const quoteNum = Math.floor(Math.random() * allQuotes.length);
   console.log(quoteNum);
   const quote = allQuotes[quoteNum];
-  console.log(quote);
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+  showQuote();
 }
 
 async function getQuotes() {
+  showLoader();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
@@ -17,3 +48,8 @@ async function getQuotes() {
 }
 
 getQuotes();
+
+function twitterPost() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
